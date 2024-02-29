681068886ae1a883fd8cab6daf54c722fde6f15e
Aditi <aditi.patre@gmail.com>
2024-02-29 16:49:28 +0530

    Nav bar changes in the existing changes updated the below: - src/App.css - sideBar.css

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 import Grid from "@mui/material/Grid";
 import DataTable from "./Components/DataTable/dataTable";
 import SideBar  from "./Components/SideBar/sideBar.js";
+import NavBar from "./Components/NavBar/navBar.js";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Grid container xs={12}>
         <Grid container alignItems="flex-end" xs={8}>
           {/* <DataTable /> */}
---
+++ src/Components/NavBar/navBar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import logo from '../../Assets/Images/logo.jpeg'
+import '../NavBar/navBar.css'; // Import CSS for styling
+
+const navBar = () => {
+    return (
+      <div className="navbar">
+        <div className="logo-container">
+          <img src={logo} alt="Logo" className="logo" />
+        </div>
+        <div class="parentDiv">
+        <div className="search-info">Search info: &nbsp;&nbsp;</div>
+        <div className="search-container">
+        
+          <input type="text" placeholder="Enter Key word" className="search-bar" />
+        </div>
+        <div className="ok-button">OK</div>
+        <div className="logout-container">  &nbsp;|&nbsp;&nbsp;
+          <a href="/eng" className="logout-link">eng</a>&nbsp;&nbsp;
+        </div>
+        <div className="logout-container">
+          <a href="/eng" className="logout-link">deu</a>&nbsp;&nbsp;
+        </div>
+        <div className="logout-container">
+          <a href="/eng" className="logout-link">fra</a>&nbsp;&nbsp;
+        </div>
+        <div className="logout-container">
+          <a href="/eng" className="logout-link">ita</a>&nbsp;|&nbsp;&nbsp;
+        </div>
+        <div className="logout-container">
+          <a href="/logout" className="logout-link">Logout</a>
+        </div>
+        </div>
+      </div>
+    );
+  };
+  
+  export default navBar;
