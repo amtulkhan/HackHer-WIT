d8e8d86224d6bfe9e5a75792165e99a906619679
Prapti Maheshwari <praptimaheshwari@Praptis-MacBook-Air.local>
2024-02-29 21:17:22 +0530

    added inputform

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import "./App.css";
 import Grid from "@mui/material/Grid";
 import DataTable from "./Components/DataTable/dataTable";
 import SideBar  from "./Components/SideBar/sideBar.js";
-
+import NavBar from "./Components/Navbar/navbar.js"
+import InputForm from "./Components/InputForm/inputForm.js";
+import { useState } from "react";
 function App() {
   const [middleContent, setMiddleContent] = useState("landing");
   const handleMiddleContent = () => {
     if (middleContent === "table") {
       return <DataTable />;
     } else if (middleContent === "landing") {
-      console.log("home");
-      return "Welcome Home";
+      
+      return <InputForm/> ;
     } else {
       console.log("review");
       return "Review Page";
@@ -29,11 +31,12 @@ function App() {
           alignItems="baseline"
           justifyContent="space-between"
           xs={12}
+          spacing={3}
         >
           <Grid item xs={2}>
             <SideBar />
           </Grid>
-          <Grid item xs={8}>
+          <Grid item xs={8} justifyContent="center">
             {handleMiddleContent()}
           </Grid>
         </Grid>
