ae4fa67652811d807aa1d5a4288d570d28b86a25
AmtulKhan <amtulmaswaraahmed@gmail.com>
2024-02-29 16:11:48 +0530

    sidebar and datatable placed together

--- src/App.js
+++ src/App.js
@@ -2,16 +2,29 @@ import logo from "./logo.svg";
 import "./App.css";
 import Grid from "@mui/material/Grid";
 import DataTable from "./Components/DataTable/dataTable";
-import SideBar  from "./Components/SideBar/sideBar.js";
+import SideBar from "./Components/Sidebar/sideBar.js";
+import NavBar from "./Components/Navbar/navbar.js";
 
 function App() {
   return (
     <div className="App">
-      <Grid container xs={12}>
-        <Grid container alignItems="flex-end" xs={8}>
-          {/* <DataTable /> */}
-          <SideBar/>
-
+      <Grid container xs={12} direction="column" justifyContent="center">
+        <Grid item xs={12}>
+          <NavBar />
+        </Grid>
+        <Grid
+          container
+          direction="row"
+          alignItems="baseline"
+          justifyContent="space-between"
+          xs={12}
+        >
+          <Grid item xs={2}>
+            <SideBar />
+          </Grid>
+          <Grid item xs={8}>
+            <DataTable />
+          </Grid>
         </Grid>
       </Grid>
     </div>
